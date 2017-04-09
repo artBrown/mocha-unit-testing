a59976514f7bfc88b1c0ba1ccf8f89f7d98b350b
Art Brown <art.brown@qutet.com>
2017-04-09 16:57:58 -0700

    node version r1

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -1,9 +1,17 @@
 //  Uncomment if using Node
-// var chai = require('chai');
+var chai = require('chai');//
 var assert = chai.assert;
 
-describe('Array', function() {
+describe('Array', function() { // es5
   it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
+
+describe('Array', () => {      // es6
+  it('should start empty', () => {
     var arr = [];
 
     assert.equal(arr.length, 0);
--- test/classNameTest.js
+++ test/classNameTest.js
@@ -1,21 +1,21 @@
 // Uncomment if using Node
-// var chai = require('chai');
+var chai = require('chai');// 
 var assert = chai.assert;
 
-// var className = require('../js/className.js');
-// var addClass = className.addClass;
+var className = require('../js/className.js');// 
+var addClass = className.addClass;// 
 
-describe('addClass', function() {
-  it('should add class into element', function() {
-    var element = { className: '' };
+describe('addClass', function () {
+  it('should add class into element', function () {
+    var element = {className: ''};
 
     addClass(element, 'test-class');
 
     assert.equal(element.className, 'test-class');
   });
 
-  it('should not add a class which already exists in element', function() {
-    var element = { className: 'exists' };
+  it('should not add a class which already exists in element', function () {
+    var element = {className: 'exists'};
 
     addClass(element, 'exists');
 
@@ -23,8 +23,8 @@ describe('addClass', function() {
     assert.equal(numClasses, 1);
   });
 
-  it('should append new class after existing one', function() {
-    var element = { className: 'exists' };
+  it('should append new class after existing one', function () {
+    var element = {className: 'exists'};
 
     addClass(element, 'new-class');
 
